6b74d90ca260e24703c1d311dc9ff2765da3cb0a
Profour <rcobb85@gmail.com>
2019-10-06 16:34:39 -0600

    Worked on cleaning up the code in ProcessorYML.

--- src/dev/nifi/yml/ProcessorYML.java
+++ src/dev/nifi/yml/ProcessorYML.java
@@ -11,6 +11,7 @@ import org.apache.nifi.api.toolkit.model.FunnelEntity;
 import org.apache.nifi.api.toolkit.model.PortEntity;
 import org.apache.nifi.api.toolkit.model.PositionDTO;
 import org.apache.nifi.api.toolkit.model.ProcessGroupEntity;
+import org.apache.nifi.api.toolkit.model.ProcessorConfigDTO;
 import org.apache.nifi.api.toolkit.model.ProcessorEntity;
 import org.apache.nifi.api.toolkit.model.PropertyDescriptorDTO;
 
@@ -79,20 +80,85 @@ public class ProcessorYML {
 	 */
 	public List<InputConnectionYML> inputs;
 	
-	
-	public ProcessorYML(ProcessorEntity pg, String dependencyReference, List<ConnectionEntity> inputs) {
+	/*
+	 * Private constructor that handles all of the common functionality any visible element on a canvas will need
+	 */
+	private ProcessorYML(String id, String name, String type, PositionDTO position, List<ConnectionEntity> inputs) {
 		// Get the unique identifiers for this processor
-		this.id = pg.getId();
-		this.name = pg.getComponent().getName();
+		this.id = id;
+		this.name = name;
 		
-		if (!this.name.equals(dependencyReference)) {
-			this.type = dependencyReference;
+		if (this.name == null || !this.name.equals(type)) {
+			this.type = type;
 		}
-				
-		// Populate the list of properties that have changed
+		
+		handlePosition(position);
+		handleConnections(inputs);
+	}
+	
+	/**
+	 * Creates a YAML representation for a single ProcessorEntity
+	 * 
+	 * @param pg Processor configuration data from NiFi's API
+	 * @param dependencyReference Canonicalized name for this type of Processor 
+	 * @param inputs All input connections for this Processor
+	 */
+	public ProcessorYML(ProcessorEntity pg, String dependencyReference, List<ConnectionEntity> inputs) {
+		this(pg.getId(), pg.getComponent().getName(), dependencyReference, pg.getPosition(), inputs);
+		
+		final ProcessorConfigDTO config = pg.getComponent().getConfig();
+		
+		// Handle all of the configurable aspects of a Processor and store the deltas
+		handleProperties(config.getDescriptors(), config.getProperties());
+		handleAnnotations(config.getAnnotationData());
+		
+		// Extract Scheduling details that may have changed for this processor
+		// TODO:
+	}
+
+	/**
+	 * Creates a YAML representation for a single ProcessGroup
+	 * 
+	 * @param pg ProcessGroup configuration data from NiFi's API
+	 * @param templateName The name of the yaml template that was generate for all of the innards of the ProcessGroup
+	 * @param inputs All input connections for this ProcessGroup
+	 */
+	public ProcessorYML(ProcessGroupEntity pg, String templateName, List<ConnectionEntity> inputs) {
+		this(pg.getId(), pg.getComponent().getName(), ReservedComponents.PROCESS_GROUP.name(), pg.getPosition(), inputs);
+		
+		// Reference the template that was generated to handle the innards of the ProcessGroup
+		this.template = templateName + ".yaml";
+		this.comment = pg.getComponent().getComments();
+	}
+	
+	/**
+	 * Creates a YAML representation for a single input/output port
+	 * 
+	 * @param port Port configuration data from NiFi's API
+	 * @param inputs All input connections for this port
+	 */
+	public ProcessorYML(PortEntity port, List<ConnectionEntity> inputs) {
+		this(port.getId(), port.getComponent().getName(), port.getPortType(), port.getPosition(), inputs);
+	}
+
+	/**
+	 * Creates a YAML representation for a single funnel
+	 * 
+	 * @param f Funnel configuration data from NiFi's API
+	 * @param inputs All input connections for this funnel
+	 */
+	public ProcessorYML(FunnelEntity f, List<ConnectionEntity> inputs) {
+		this(f.getId(), null, ReservedComponents.FUNNEL.name(), f.getPosition(), inputs);
+	}
+
+	/*
+	 * Helper method that checks all properties and looks for any configures values that differ from the default value.
+	 */
+	private void handleProperties(Map<String, PropertyDescriptorDTO> defaultProperties, 
+								  Map<String, String> configuredValues) {
 		this.properties = new HashMap<String, String>();
-		Map<String, PropertyDescriptorDTO> defaultProperties = pg.getComponent().getConfig().getDescriptors();
-		Map<String, String> configuredValues = pg.getComponent().getConfig().getProperties();
+
+		// Populate the list of properties that have changed (compare default value vs configured value)
 		for (String propertyName : defaultProperties.keySet()) {
 			PropertyDescriptorDTO defaultProperty = defaultProperties.get(propertyName);
 			String configuredValue = configuredValues.get(propertyName);
@@ -104,69 +170,11 @@ public class ProcessorYML {
 				System.out.println("Configured Value: " + propertyName + " = " + configuredValue);
 			}
 		}
-		
-		// Check to see if there are any advanced annotations on this processor
-		String xmlAnnotations = pg.getComponent().getConfig().getAnnotationData();
-		if (xmlAnnotations != null && !xmlAnnotations.isEmpty()) {
-			this.rules = new ArrayList<>();
-			
-			XmlMapper xmlMapper = new XmlMapper();
-		    try {
-				Criteria criteria = xmlMapper.readValue(xmlAnnotations, Criteria.class);
-				
-				this.rulesPolicy = criteria.flowFilePolicy;
-				
-				for (Rules rule : criteria.rules) {
-					RulesYML r = new RulesYML(rule.conditions, rule.actions);
-					
-					this.rules.add(r);
-				}
-				
-			} catch (IOException e) {
-				// TODO: setup proper logging
-				System.out.println("Failed to parse xml: " + xmlAnnotations);
-				e.printStackTrace();
-			}
-		}
-		
-		// Extract Scheduling details that may have changed for this processor
-		// TODO:
-
-		handlePosition(pg.getPosition());
-		handleConnections(inputs);
 	}
 
-
-	public ProcessorYML(FunnelEntity f, List<ConnectionEntity> inputs) {
-		this.id = f.getId();
-		this.type = ReservedComponents.FUNNEL.name();
-
-		handlePosition(f.getPosition());
-		handleConnections(inputs);
-	}
-
-
-	public ProcessorYML(ProcessGroupEntity pg, String templateName, List<ConnectionEntity> inputs) {
-		this.id = pg.getId();
-		this.type = ReservedComponents.PROCESS_GROUP.name();
-		this.name = pg.getComponent().getName();
-		this.template = templateName + ".yaml";
-		this.comment = pg.getComponent().getComments();
-		
-		handlePosition(pg.getPosition());
-		handleConnections(inputs);
-	}
-	
-	public ProcessorYML(PortEntity port, List<ConnectionEntity> inputs) {
-		this.id = port.getId();
-		this.type = port.getPortType();
-		this.name = port.getComponent().getName();
-
-		handlePosition(port.getPosition());
-		handleConnections(inputs);
-	}
-
-
+	/*
+	 * Helper method to store all input connections to a processor
+	 */
 	private void handleConnections(List<ConnectionEntity> inputs) {
 
 		// Process incoming connections to fill out the input listing
@@ -178,9 +186,43 @@ public class ProcessorYML {
 		}
 	}
 	
+	/*
+	 * Helper method to create a consistent storage format for positional data
+	 */
 	private void handlePosition(PositionDTO position) {
 		// Truncate position down to integer values
 		this.position = String.format("%.0f,%.0f", position.getX(), position.getY());
+	}
+	
+	/*
+	 * Helper method to convert XML annotation data into a storable YAML format.
+	 * This only applies to the advanced tab on a processor where Rules/Conditions/Actions are configured.
+	 */
+	private void handleAnnotations(String xmlAnnotations) {
+		// Check to see if there are any annotations, if not just bail out early
+		if (xmlAnnotations == null || xmlAnnotations.isEmpty()) {
+			return;
+		}
+		
+		this.rules = new ArrayList<>();
+		
+		XmlMapper xmlMapper = new XmlMapper();
+	    try {
+			Criteria criteria = xmlMapper.readValue(xmlAnnotations, Criteria.class);
+			
+			this.rulesPolicy = criteria.flowFilePolicy;
+			
+			for (Rules rule : criteria.rules) {
+				RulesYML r = new RulesYML(rule.conditions, rule.actions);
+				
+				this.rules.add(r);
+			}
+			
+		} catch (IOException e) {
+			// TODO: setup proper logging
+			System.out.println("Failed to parse xml: " + xmlAnnotations);
+			e.printStackTrace();
+		}
 	}
 
 }
